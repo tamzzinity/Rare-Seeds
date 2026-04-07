4c5c9b1bfb8c19eb518898ccc0c1918c9525d05e
tamzz <nizamuddintaimur@gmail.com>
2026-04-07 16:18:48 +0100

    Create SeedData.js with stone spawn coordinates
    Added seed data for various stone types with coordinates.

---
+++ SeedData.js
@@ -0,0 +1,15 @@
+// Average Stuff
+let stoneSpawn = "63, 58";
+console.log(stoneSpawn);
+
+let deepslateSpawn = "1, -15";
+console.log(stoneSpawn);
+
+let graniteSpawn = "0, 60";
+console.log(graniteSpawn);
+
+let andesiteSpawn = "60, 2";
+console.log(andesiteSpawn);
+
+let dioriteSpawn = "54, .4";
+const.log(dioriteSpawn);
