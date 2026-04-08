eebbf41591c2fd80de49e71c85e770315c947aa3
tamzz <nizamuddintaimur@gmail.com>
2026-04-08 17:01:54 +0100

    Update SeedData.js

--- SeedData.js
+++ SeedData.js
@@ -14,6 +14,23 @@ console.log("Andesite Spawn" + andesiteSpawn);
 let dioriteSpawn = "Random Areas";
 console.log("Diorite Spawn" + dioriteSpawn);
 
-console.log("Updated Everyday")
-const updating = true
+let bedrockSpawn = "-64";
+console.log("Bedrock Spawn" + bedrockSpawn);
+
+// File info
+console.log("Updated Everyday");
+const updating = true;
+console.log("updating:" + updating);
+
+// All of our seed's versions:
+const version = "1.20.3";
+console.log("Version of seeds:" + version);
+console.log("If you are wondering why?: It is easiest on this version according to us. (not all of you guys may expect it.))";
+      
+
+
+
+
+
+
 
